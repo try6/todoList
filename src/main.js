083a76d6b0407cabde610d610cd8fb2790da54f7
TRY <522263680@qq.com>
2018-03-06 17:18:10 +0800

    'bug修复'

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 
-import router from './router'
+/* import router from './router' */
 
 
 /* import "./style/main.css" */
@@ -13,7 +13,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
-    router,
     template: '<App/>',
     components: {
         App
